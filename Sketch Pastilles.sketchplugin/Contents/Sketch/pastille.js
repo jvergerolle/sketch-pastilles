0719fd2fda502cf8246ba6a6b1da9a7c197c339e
Jimmy Vergerolle <jvergerolle@ipedis.com>
2017-01-27 15:54:16 +0100

    Move imports

--- Sketch Pastilles.sketchplugin/Contents/Sketch/pastille.js
+++ Sketch Pastilles.sketchplugin/Contents/Sketch/pastille.js
@@ -1,12 +1,12 @@
+//Libraries
+@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/config.js'
+@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/lib/utils.js'
+@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/create/Group.js'
+@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/create/Rectangle.js'
+@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/create/Textfield.js'
+
 var onRun = function(context)
 {
-	//Libraries
-	@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/config.js'
-	@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/lib/utils.js'
-	@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/create/Group.js'
-	@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/create/Rectangle.js'
-	@import '~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/Sketch Pastilles.sketchplugin/Contents/Sketch/create/Textfield.js'
-
 	//get javascript API root object
 	var sketch = context.api();
 
@@ -20,7 +20,7 @@ var onRun = function(context)
 		init(conf);
 	}
 	catch(error){
-		context.api().alert(error.message,"Javascript Error");
+		context.api().alert(error.message,"CocoaScript Error");
 	}
 
 	//Define init
